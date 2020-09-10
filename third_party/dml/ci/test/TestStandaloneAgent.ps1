466de5c8a44086865b898df11eaecce4c4001230
Patrice Vignola <pavignol@microsoft.com>
2020-09-10 12:42:25 -0700

    WIP

--- third_party/dml/ci/test/TestStandaloneAgent.ps1
+++ third_party/dml/ci/test/TestStandaloneAgent.ps1
@@ -7,7 +7,7 @@ param
     [string]$ArtifactDirectory = $env:System_ArtifactsDirectory,
     [Parameter(Mandatory=$true)][string[]]$TestGroups,
     [Parameter(Mandatory=$true)][string[]]$TestArtifacts,
-    [Parameter(Mandatory=$true)][string[]]$BuildArtifcatsBranch
+    [Parameter(Mandatory=$true)][string[]]$BuildArtifactsBranch
 )
 
 # In case the caller passed in a single comma-separated string, such as 'models, python', instead of
@@ -31,7 +31,7 @@ $UriParameters = @(
     "`$top=1",
     "definitions=$BuildPipelineID",
     "queryOrder=queueTimeDescending",
-    "branchName=refs/heads/$BuildArtifcatsBranch",
+    "branchName=refs/heads/$BuildArtifactsBranch",
     "statusFilter=completed",
     "resultFilter=succeeded",
     "api-version=5.0"
@@ -40,7 +40,7 @@ $UriParameters = @(
 $Build = $Ado.InvokeProjectApi("build/builds?$UriParameters", "GET", $null).Value
 if (!$Build)
 {
-    Write-Warning "Could not find last successful build of $BuildArtifcatsBranch branch."
+    Write-Warning "Could not find last successful build of $BuildArtifactsBranch branch."
     exit 1
 }
 
