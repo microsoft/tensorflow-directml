8b0c62ceacbe34b4e75df40bcf96cd40b6d98012
Justin Stoecker <justoeck@microsoft.com>
2020-09-15 11:27:02 -0700

    Don't include winadapter.h in Windows

--- third_party/dml/winadapter/dxguids.cc
+++ third_party/dml/winadapter/dxguids.cc
@@ -3,6 +3,8 @@
 
 // This file's sole purpose is to initialize the GUIDs declared using the DEFINE_GUID macro.
 #define INITGUID
+#ifndef _WIN32
 #include "winadapter.h"
+#endif
 #include "dxcore.h"
 #include "d3d12.h"
