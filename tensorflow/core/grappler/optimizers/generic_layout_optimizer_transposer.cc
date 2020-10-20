0b2c41d6bc6b99a813f23d90322c90c68f6841cf
Patrice Vignola <vignola.patrice@gmail.com>
2020-10-20 11:46:14 -0700

    Add clarifying comment

--- tensorflow/core/grappler/optimizers/generic_layout_optimizer_transposer.cc
+++ tensorflow/core/grappler/optimizers/generic_layout_optimizer_transposer.cc
@@ -776,6 +776,10 @@ Status FusedBatchNormGradTransposer::TransposeNode(
 
   if (!IsTraining(*node)) {
     string task, device;
+
+    // Unlike the CUDA kernel, DML's FusedBatchNormGrad should always be faster
+    // with NCHW, so we transform the layout here instead of doing it
+    // in the kernel itself.
     bool is_on_dml = DeviceNameUtils::SplitDeviceName(node->node()->device(),
                                                       &task, &device) &&
                      absl::StartsWith(device, DEVICE_DML);
