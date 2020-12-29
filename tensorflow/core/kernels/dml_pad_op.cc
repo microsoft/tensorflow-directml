10252740a86e0cf893d1a7b470051aa025e4f20e
Patrice Vignola <vignola.patrice@gmail.com>
2020-12-29 12:09:05 -0800

    WIP

--- tensorflow/core/kernels/dml_pad_op.cc
+++ tensorflow/core/kernels/dml_pad_op.cc
@@ -49,6 +49,7 @@ struct SimplePad {
 template <typename Tpadding>
 absl::optional<SimplePad> SimplifyPad(const TensorShape& input_shape,
                                       const Tensor& paddings_tensor,
+                                      bool constant_pad,
                                       size_t min_output_size = 4,
                                       size_t max_output_size = 5) {
   auto paddings = paddings_tensor.matrix<Tpadding>();
@@ -64,14 +65,21 @@ absl::optional<SimplePad> SimplifyPad(const TensorShape& input_shape,
 
     // Coalesce subsequent non-padded dims into the current dim.
     int j = i + 1;
-    while (j < paddings.dimension(0) && paddings(j, 0) == 0 &&
-           paddings(j, 1) == 0) {
-      auto other_dim_size = static_cast<uint32_t>(input_shape.dim_size(j));
-      size *= other_dim_size;
-      start_pad *= other_dim_size;
-      end_pad *= other_dim_size;
-      j++;
+
+    // Constant padding can collapse adjacent non-padded and padded dimensions
+    // because there's a single value to be padded, but the padding values for
+    // reflect and symmetric depend on the shape.
+    if (constant_pad || (paddings(i, 0) == 0 && paddings(i, 1) == 0)) {
+      while (j < paddings.dimension(0) && paddings(j, 0) == 0 &&
+             paddings(j, 1) == 0) {
+        auto other_dim_size = static_cast<uint32_t>(input_shape.dim_size(j));
+        size *= other_dim_size;
+        start_pad *= other_dim_size;
+        end_pad *= other_dim_size;
+        j++;
+      }
     }
+
     i = j;
 
     simple_pad.in_shape.push_back(size);
@@ -194,11 +202,12 @@ class PadInitHelper : public InitializationHelper {
       output_shape_.AddDim(before_d + size_d + after_d);
     }
 
-    simple_pad_ = SimplifyPad<Tpadding>(input.shape(), paddings);
+    bool constant_pad = padding_mode_ == DML_PADDING_MODE_CONSTANT;
+    simple_pad_ = SimplifyPad<Tpadding>(input.shape(), paddings, constant_pad);
     OP_REQUIRES(ctx, simple_pad_.has_value(),
                 errors::InvalidArgument(
-                    "DML can only handle up to 5D padding, but the given shape "
-                    "and paddings cannot be simplified to 5D."));
+                    "DML can only handle up to 8D padding, but the given shape "
+                    "and paddings cannot be simplified to 8D."));
   }
 
   const TensorShape& GetOutputShape() const { return output_shape_; }
