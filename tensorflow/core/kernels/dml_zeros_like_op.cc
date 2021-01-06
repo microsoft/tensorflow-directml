bd22631951cdce15b569ce55614d18eea6b474eb
Patrice Vignola <vignola.patrice@gmail.com>
2021-01-05 19:20:59 -0800

    WIP

--- tensorflow/core/kernels/dml_zeros_like_op.cc
+++ tensorflow/core/kernels/dml_zeros_like_op.cc
@@ -20,11 +20,15 @@ limitations under the License.
 #include "tensorflow/core/kernels/data/optional_ops.h"
 #include "tensorflow/core/kernels/dml_kernel_wrapper.h"
 #include "tensorflow/core/kernels/dml_ops_common.h"
+#include "tensorflow/core/kernels/list_kernels.h"
 
 namespace tensorflow {
 
 static Status DmlUnaryOpVariant(OpKernelContext* ctx, VariantUnaryOp op,
                                 const Variant& v, Variant* v_out);
+
+template <typename T>
+static Status DmlVariantZerosLike(OpKernelContext* ctx, const T& x, T* y);
 
 static void SetTensorToZero(OpKernelContext* ctx, const Tensor& tensor) {
   DmlDevice* device = static_cast<DmlDevice*>(ctx->device());
@@ -74,9 +78,10 @@ static Status DmlZerosLikeTensor(OpKernelContext* ctx, const Tensor& x,
   return Status::OK();
 }
 
-static Status DmlOptionalZerosLike(OpKernelContext* ctx,
-                                   const data::OptionalVariant& x,
-                                   data::OptionalVariant* y) {
+template <>
+static Status DmlVariantZerosLike<data::OptionalVariant>(
+    OpKernelContext* ctx, const data::OptionalVariant& x,
+    data::OptionalVariant* y) {
   if (!x.has_value()) {
     *y = x;
     return Status::OK();
@@ -91,18 +96,33 @@ static Status DmlOptionalZerosLike(OpKernelContext* ctx,
   return Status::OK();
 }
 
+template <>
+static Status DmlVariantZerosLike<TensorList>(OpKernelContext* c,
+                                              const TensorList& x,
+                                              TensorList* y) {
+  y->element_dtype = x.element_dtype;
+  y->element_shape = x.element_shape;
+  y->tensors().reserve(x.tensors().size());
+  for (const Tensor& t : x.tensors()) {
+    Tensor out_tensor;
+    TF_RETURN_IF_ERROR(DmlZerosLikeTensor(c, t, &out_tensor));
+    y->tensors().emplace_back(out_tensor);
+  }
+  return Status::OK();
+}
+
 static Status DmlUnaryOpVariant(OpKernelContext* ctx, VariantUnaryOp op,
                                 const Variant& v, Variant* v_out) {
-  DCHECK_NE(v_out, nullptr);
-  *v_out = data::OptionalVariant();
-  if (v.get<data::OptionalVariant>() == nullptr) {
+  UnaryVariantOpRegistry::VariantUnaryOpFn* unary_op_fn =
+      UnaryVariantOpRegistry::Global()->GetUnaryOpFn(op, DEVICE_DML,
+                                                     v.TypeId());
+  if (unary_op_fn == nullptr) {
     return errors::Internal(
-        "VariantUnaryOpFn: Could not access object, type_index: ",
-        port::MaybeAbiDemangle(MakeTypeIndex<data::OptionalVariant>().name()));
+        "No unary variant unary_op function found for unary variant op enum: ",
+        op, " Variant type_name: ", v.TypeName(),
+        " for device type: ", DEVICE_DML);
   }
-  const data::OptionalVariant& t = *v.get<data::OptionalVariant>();
-  data::OptionalVariant* t_out = v_out->get<data::OptionalVariant>();
-  return DmlOptionalZerosLike(ctx, t, t_out);
+  return (*unary_op_fn)(ctx, v, v_out);
 }
 
 class DmlZerosLikeKernel : public OpKernel {
@@ -110,29 +130,34 @@ class DmlZerosLikeKernel : public OpKernel {
   explicit DmlZerosLikeKernel(OpKernelConstruction* ctx) : OpKernel(ctx) {}
 
   void Compute(OpKernelContext* ctx) override {
+    Tensor* output_tensor = nullptr;
+    OP_REQUIRES_OK(
+        ctx, ctx->allocate_output(0, ctx->input(0).shape(), &output_tensor));
+
+    SetTensorToZero(ctx, *output_tensor);
+  }
+};
+
+class DmlZerosLikeKernelVariant : public OpKernel {
+ public:
+  explicit DmlZerosLikeKernelVariant(OpKernelConstruction* ctx)
+      : OpKernel(ctx) {}
+
+  void Compute(OpKernelContext* ctx) override {
     const Tensor& input = ctx->input(0);
 
-    if (input.dtype() == DT_VARIANT) {
-      OP_REQUIRES(
-          ctx, input.dims() == 0,
-          errors::InvalidArgument("ZerosLike non-scalar Tensor with "
-                                  "dtype=DT_VARIANT is not supported."));
-      const Variant& v = input.scalar<Variant>()();
-      // DT_VARIANT tensors must be allocated on CPU since they wrap C++
-      // objects which can not be efficiently represented in GPU memory.
-      int numa_node = DeviceNumaNode(ctx->device());
-      Tensor out(cpu_allocator(numa_node), DT_VARIANT, TensorShape({}));
-      Variant* out_v = &(out.scalar<Variant>()());
-      OP_REQUIRES_OK(
-          ctx, DmlUnaryOpVariant(ctx, ZEROS_LIKE_VARIANT_UNARY_OP, v, out_v));
-      ctx->set_output(0, out);
-    } else {
-      Tensor* output_tensor = nullptr;
-      OP_REQUIRES_OK(ctx,
-                     ctx->allocate_output(0, input.shape(), &output_tensor));
-
-      SetTensorToZero(ctx, *output_tensor);
-    }
+    OP_REQUIRES(ctx, input.dims() == 0,
+                errors::InvalidArgument("ZerosLike non-scalar Tensor with "
+                                        "dtype=DT_VARIANT is not supported."));
+    const Variant& v = input.scalar<Variant>()();
+    // DT_VARIANT tensors must be allocated on CPU since they wrap C++
+    // objects which can not be efficiently represented in GPU memory.
+    int numa_node = DeviceNumaNode(ctx->device());
+    Tensor out(cpu_allocator(numa_node), DT_VARIANT, TensorShape({}));
+    Variant* out_v = &(out.scalar<Variant>()());
+    OP_REQUIRES_OK(
+        ctx, DmlUnaryOpVariant(ctx, ZEROS_LIKE_VARIANT_UNARY_OP, v, out_v));
+    ctx->set_output(0, out);
   }
 };
 
@@ -143,7 +168,19 @@ class DmlZerosLikeKernel : public OpKernel {
 
 // TODO(b/25387198): A special kernel exists for int32 (see constant_op.cc).
 TF_CALL_DML_ALL_TYPES_EXCEPT_INT32(REGISTER_DML_KERNEL)
-TF_CALL_variant(REGISTER_DML_KERNEL)
 #undef REGISTER_DML_KERNEL
+
+REGISTER_KERNEL_BUILDER(
+    Name("ZerosLike").Device(DEVICE_DML).TypeConstraint<Variant>("T"),
+    DmlZerosLikeKernelVariant);
+
+#define REGISTER_VARIANT_DML_KERNEL(TYPE)                               \
+  REGISTER_UNARY_VARIANT_UNARY_OP_FUNCTION(ZEROS_LIKE_VARIANT_UNARY_OP, \
+                                           DEVICE_DML, TYPE,            \
+                                           DmlVariantZerosLike<TYPE>);
+
+REGISTER_VARIANT_DML_KERNEL(data::OptionalVariant)
+REGISTER_VARIANT_DML_KERNEL(TensorList)
+#undef REGISTER_VARIANT_DML_KERNEL
 
 }  // namespace tensorflow
