abb28ec9cbc2a335931b2b479c533f1fa8d86c79
Patrice Vignola <vignola.patrice@gmail.com>
2021-05-05 03:15:51 -0700

    WIP

--- tensorflow/python/kernel_tests/distributions/special_math_test.py
+++ tensorflow/python/kernel_tests/distributions/special_math_test.py
@@ -345,8 +345,6 @@ class NdtrGradientTest(test.TestCase):
           rtol=error_spec.rtol,
           atol=error_spec.atol)
 
-  # DML's implementation for erfc is 1.0 - erf(x), which isn't precise enough to
-  # represent large values of x
   @test_util.run_deprecated_v1
   def test_float32(self):
     self._test_grad_accuracy(np.float32, self._grid, self._error32)
