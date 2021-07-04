5b4b89081f6a438988f80fb8455f912616047a22
Patrice Vignola <vignola.patrice@gmail.com>
2021-07-04 11:24:54 -0700

    WIP

--- third_party/dml/ci/tests/tfdml_test_runner.py
+++ third_party/dml/ci/tests/tfdml_test_runner.py
@@ -197,13 +197,18 @@ def main():
           # Don't gather device placement data on WSL for now
           log_device_placement = os.name == "nt" and args.log_device_placement
 
-          executor = sequential_executor if _is_distribute_test(
-              exe_path) else parallel_executor
-
-          futures.append(
-              executor.submit(_run_test, exe_path, log_device_placement,
-                              shard_index, shard_count, args.test_framework,
-                              args.test_timeout))
+          if _is_distribute_test(exe_path):
+            futures.append(
+                sequential_executor.submit(_run_test, exe_path,
+                                           log_device_placement, shard_index,
+                                           shard_count, args.test_framework,
+                                           args.test_timeout))
+          else:
+            futures.append(
+                parallel_executor.submit(_run_test, exe_path,
+                                         log_device_placement, shard_index,
+                                         shard_count, args.test_framework,
+                                         args.test_timeout))
 
       for future in futures:
         future.result()
