b13b3a79ecc0131957f05229c850c13cc88fd9f2
Patrice Vignola <vignola.patrice@gmail.com>
2021-07-12 12:11:32 -0700

    Update portpicker to the latest pip version

--- third_party/dml/ci/python_portpicker/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -ex
-
-unset PYTHONPATH
-python3 -m venv build/venv
-. build/venv/bin/activate
-
-pip install --upgrade pip
-pip install tox
-tox -e "py3$(python -c 'import sys; print(sys.version_info.minor)')"
