628dbd9f146810df93525fa4e1553a1de7962c9e
Patrice Vignola <vignola.patrice@gmail.com>
2022-05-02 11:22:50 -0700

    Increase the default kernel max cache size to 1536 (#370)

--- tensorflow/core/common_runtime/dml/dml_kernel_manager.h
+++ tensorflow/core/common_runtime/dml/dml_kernel_manager.h
@@ -49,7 +49,7 @@ class NoOpInitializationHelper;
 class DmlKernelManager {
  public:
   // Can be overridden by the TF_DIRECTML_KERNEL_CACHE_SIZE environment variable
-  static constexpr size_t kDefaultMaxCacheSize = 1024;
+  static constexpr size_t kDefaultMaxCacheSize = 1536;
 
   DmlKernelManager();
 
