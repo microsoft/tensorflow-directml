4221fdae4d597155a55e1d48590cfbe62cea10c7
Patrice Vignola <vignola.patrice@gmail.com>
2022-05-05 19:46:40 -0700

    Update DirectML to version 1.8.2 and prepare TF for 1.15.6 release (#371)

--- tensorflow/tools/pip_package/setup.py
+++ tensorflow/tools/pip_package/setup.py
@@ -52,7 +52,7 @@ DOCLINES = __doc__.split('\n')
 # result for pip.
 # Also update tensorflow/tensorflow.bzl and
 # tensorflow/core/public/version.h
-_VERSION = '1.15.5.dev0'
+_VERSION = '1.15.6.dev0'
 
 REQUIRED_PACKAGES = [
     'absl-py >= 0.7.0',
